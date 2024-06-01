2ad1a90df49406d66e951c07a7997ee8c0b407df
oscarcarranzaa <oscarcarranza504@gmail.com>
2024-06-01 14:44:33 -0600

    Delete tinymce

--- src/components/publish/publishEditor/index.tsx
+++ src/components/publish/publishEditor/index.tsx
@@ -1,12 +1,7 @@
 'use client'
 import { Input } from '@nextui-org/react'
-import dynamic from 'next/dynamic'
 
 export default function PublishEditor() {
-  const EditorText = dynamic(() => import('@/components/EditorText/'), {
-    ssr: false,
-    loading: () => <p>Cargando....</p>,
-  })
   return (
     <>
       <div className="grid grid-cols-12">
@@ -14,9 +9,6 @@ export default function PublishEditor() {
           <Input variant="bordered" isRequired label="Titulo" />
         </div>
         <div></div>
-      </div>
-      <div className="mt-10 p-3">
-        <EditorText />
       </div>
     </>
   )

eabb4ef86fde38696bafcf4af204d94bb250c50c
oscarcarranzaa <oscarcarranza504@gmail.com>
2024-11-01 18:58:39 -0600

    Si ya tiene sesion que no pueda ver el login

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,30 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('updateToken')
+  const tokenValue = token?.value
 
-  if (token?.value === undefined) {
+  // Si el usuario intenta acceder a `/login` y ya tiene un token, redirigir a `/dash`
+  if (request.nextUrl.pathname === '/login' && tokenValue) {
+    try {
+      const secretKey = process.env.JWT_REFRESH_KEY || ''
+      const secret = new TextEncoder().encode(secretKey)
+
+      await jwtVerify(tokenValue, secret)
+      return NextResponse.redirect(new URL('/dash/dashboard', request.url))
+    } catch (error) {
+      // Si el token es inválido o expira, no se redirige y se permite el acceso a `/login`
+    }
+  }
+
+  // Si el token no existe o es inválido, redirigir al usuario a `/login` cuando intenta acceder a `/dash`
+  if (request.nextUrl.pathname.startsWith('/dash') && !tokenValue) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
-  const tokenValue = token?.value
   try {
+    // Verificar el token para rutas protegidas en `/dash`
     if (tokenValue) {
       const secretKey = process.env.JWT_REFRESH_KEY || ''
-
       const secret = new TextEncoder().encode(secretKey)
 
       await jwtVerify(tokenValue, secret)
@@ -23,6 +37,7 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 }
+
 export const config = {
-  matcher: '/dash/:path*',
+  matcher: ['/dash/:path*', '/login'],
 }

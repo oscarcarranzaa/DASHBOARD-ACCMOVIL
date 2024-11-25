efe49fca083bbab3798ba8d9ffa0cd9a7088044a
oscarcarranzaa <oscarcarranza504@gmail.com>
2024-11-25 15:56:35 -0600

    Add: security nav header

--- src/components/navegationLayout/header/menuObjects.tsx
+++ src/components/navegationLayout/header/menuObjects.tsx
@@ -1,11 +1,17 @@
+import LockSVG from '@/components/icons/lock'
 import UserSVG from '@/components/icons/user'
 
-const iconSize = 22
+const iconSize = 20
 
 export const profileItems = [
   {
     name: 'Configuración de la cuenta',
     icon: <UserSVG size={iconSize} />,
     href: '/dash/account',
+  },
+  {
+    name: 'Seguridad',
+    icon: <LockSVG size={iconSize} />,
+    href: '/dash/account/security',
   },
 ]

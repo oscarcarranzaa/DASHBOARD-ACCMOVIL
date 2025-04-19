aa3789a042a01631ccb18859071856ddca5b2905
oscarcarranzaa <oscarcarranza504@gmail.com>
2025-04-19 12:25:51 -0600

    fix dash/multimedia

--- src/app/dash/multimedia/page.tsx
+++ src/app/dash/multimedia/page.tsx
@@ -1,12 +1,15 @@
 import Media from '@/components/media'
 import NavegationPages from '@/components/navegationPages'
+import { Suspense } from 'react'
 
 export default function Multimedia() {
   return (
     <>
       <NavegationPages text="Archivos multimedia" />
       <div className="mt-10">
-        <Media />
+        <Suspense>
+          <Media />
+        </Suspense>
       </div>
     </>
   )

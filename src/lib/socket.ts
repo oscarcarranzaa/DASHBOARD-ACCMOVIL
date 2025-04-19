04b51dd8cf4ba188716b76dc77c44334f0981b01
oscarcarranzaa <oscarcarranza504@gmail.com>
2025-04-18 18:59:48 -0600

    Fix domain socket

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,7 +1,7 @@
 // lib/socket.ts
 import { io } from 'socket.io-client'
 
-export const socket = io('http://localhost:4000', {
+export const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL, {
   withCredentials: true,
   transports: ['websocket'],
 })

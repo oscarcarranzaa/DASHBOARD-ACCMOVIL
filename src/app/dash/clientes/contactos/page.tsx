fe1e25d47f5cbf82a564cecddf57058d2dfa5952
oscarcarranzaa <oscarcarranza504@gmail.com>
2025-04-18 20:40:07 -0600

    Test fix

--- src/app/dash/clientes/contactos/page.tsx
+++ src/app/dash/clientes/contactos/page.tsx
@@ -2,6 +2,7 @@ import ContactList from '@/components/customers/contacts/contactList'
 import CreateContact from '@/components/customers/contacts/newContact'
 import NavegationPages from '@/components/navegationPages'
 import Search from '@/components/UI/search'
+import { Suspense } from 'react'
 
 export default function Contactos() {
   return (
@@ -9,11 +10,15 @@ export default function Contactos() {
       <NavegationPages text="Contactos" />
       <div>
         <div className="mb-3 flex justify-between">
-          <Search placeHolder="Buscar contacto..." />
+          <Suspense>
+            <Search placeHolder="Buscar contacto..." />
+          </Suspense>
           <CreateContact />
         </div>
         <div className="mb-16">
-          <ContactList />
+          <Suspense>
+            <ContactList />
+          </Suspense>
         </div>
       </div>
     </div>
